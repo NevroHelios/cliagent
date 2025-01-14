f945ee8a8411397962a2057884f91759ff04fc52
NevroHelios <dasharka05@gmail.com>
2025-01-14 20:31:48 +0530

    Refactor code to improve commit message generation and add chat functionality

--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 
 	// "os/exec"
 	"democli/start/utils"
 	"os"
-	"regexp"
 
-	"github.com/WqyJh/go-fstring"
 	"github.com/atotto/clipboard"
 	"github.com/charmbracelet/huh"
 	"github.com/joho/godotenv"
@@ -23,7 +20,17 @@ var (
 	Prompt          string
 	AvailableModels []string
 	ModelsMapped    map[string]interface{}
+	modelOptions    []huh.Option[string]
 )
+
+func loadHuhModelOption(API_KEY string) {
+	AvailableModels = utils.GetAvailableModels(API_KEY)
+	// huh options
+	for _, model := range AvailableModels {
+		modelName := cases.Title(language.English, cases.NoLower).String(strings.Join(strings.Split(model, "-"), " "))
+		modelOptions = append(modelOptions, huh.NewOption(modelName, model))
+	}
+}
 
 func main() {
 
@@ -33,21 +40,27 @@ func main() {
 		fmt.Println("Error loading .env file", err)
 	}
 	GROQ_API_KEY := os.Getenv("GROQ_API_KEY")
-	// get available models
-	get_models_url := "https://api.groq.com/openai/v1/models"
-	AvailableModels = utils.GetAvailableModels(get_models_url, GROQ_API_KEY)
-	sort.Slice(AvailableModels, func(i, j int) bool {
-		return AvailableModels[i] < AvailableModels[j]
-	})
 
-	// huh options
-	modelOptions := []huh.Option[string]{}
-	for _, model := range AvailableModels {
-		modelName := cases.Title(language.English, cases.NoLower).String(strings.Join(strings.Split(model, "-"), " "))
-		modelOptions = append(modelOptions, huh.NewOption(modelName, model))
+	// check if the user wats to chat or commit
+	var purpose string
+	purForm := huh.NewSelect[string]().
+		Title("Whats the mood?").
+		Options(
+			huh.NewOption("chat", "chat"),
+			huh.NewOption("generate commit msg!", "commit"),
+		).
+		Value(&purpose)
+
+	err = purForm.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	form := huh.NewForm(
+	// fetch the available models
+	loadHuhModelOption(GROQ_API_KEY)
+
+	selectModelForm := huh.NewForm(
 		huh.NewGroup(
 			huh.NewSelect[string]().
 				Title("Select a Model: ").
@@ -56,59 +69,29 @@ func main() {
 				).
 				Value(&Model),
 		),
-
-		huh.NewGroup(
-			huh.NewInput().
-				Title("Something different? ").
-				Placeholder("Tell me a joke!").
-				Value(&Prompt),
-		),
 	)
 
-	runErr := form.Run()
+	runErr := selectModelForm.Run()
 	if runErr != nil {
 		fmt.Println(runErr)
 		return
 	}
+	// its onlly for the chat purpose
+	if purpose == "chat" {
+		inputForm := huh.NewInput().
+			Title("Something different? ").
+			Placeholder("Tell me a joke!").
+			Value(&Prompt)
 
-	git_diff := ""
-	if Prompt == "" {
-		// get the git diff
-		cmd, err := utils.RunCommand("git", "diff")
-		if err != nil {
-			fmt.Println("No git diff", err)
-			return
-		}
-
-		template := `
-		You are an expert software engineer assisting with writing clear and concise git commit messages. Given the following "git diff" output, analyze the changes and provide a descriptive commit message summarizing the purpose and impact of the modifications in 50 words or less. 
-		NOTE: You are to return only the commit message and nothing else.
-		
-		Git Diff:
-		{git_diff}
-		
-		Commit Message:
-		
-		`
-		lines := strings.Split(cmd, "\n")
-		if len(lines) > 2000 {
-			re := regexp.MustCompile(`^.func`)
-			for _, line := range lines {
-				if re.MatchString(line) {
-					git_diff += line + "\n"
-				}
-			}
-		} else {
-			git_diff = cmd
-		}
-
-		diff := map[string]any{"git_diff": git_diff}
-		Prompt, err = fstring.Format(template, diff)
-		if err != nil {
-			fmt.Println(err)
+		runErr := inputForm.Run()
+		if runErr != nil {
+			fmt.Println(runErr)
 			return
 		}
 	}
+
+	git_diff := ""
+	Prompt = getPrompt(purpose, Prompt)
 
 	// fmt.Println(Prompt)
 	res := utils.ModelCall(Model, Prompt, "", GROQ_API_KEY)
@@ -122,7 +105,7 @@ func main() {
 	fmt.Println(res)
 
 	// copy to clipboard
-	if git_diff != "" {
+	if git_diff != "" && purpose == "commit" {
 		clipErr := clipboard.WriteAll(res)
 		if clipErr != nil {
 			fmt.Println("failed to copy ", clipErr)
